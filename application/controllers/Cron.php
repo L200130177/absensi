b025beadc383cac33cbfe5fffae03c4b398e0339
arif <arif.setyan11@gmail.com>
2023-11-25 01:58:56 +0700

    cron job

--- application/controllers/Cron.php
+++ application/controllers/Cron.php
@@ -9,4 +9,17 @@ class Cron extends MY_Controller {
 		return $query;
 	}
 
+	public function set_token()
+	{
+		$query = $this->db->query("
+		SELECT user_id, activity
+		FROM user
+		WHERE NOW() >= activity + INTERVAL 2 HOUR");
+		foreach($query->result() as $r){
+			$this->db->set('remember_token', null);
+			$this->db->where('user_id', $r->user_id);
+			$this->db->update('user');
+		}
+	}
+
 }
